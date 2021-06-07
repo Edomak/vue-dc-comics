5582017b2d3c743726d119821969efd059360eea
Edomak <edoardo.maccherini@gmail.com>
2021-06-07 14:24:17 +0200

    Init

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <header>
+        <div class="logo">
+            <img src="" alt="Logo DC">
+        </div>
+        <nav></nav>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style>
+
+</style>
